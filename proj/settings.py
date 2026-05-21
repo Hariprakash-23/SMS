9fbfd4a7d5042992261aebf6f0a1ad2a0bb43652
Hariprakash-23 <kahariprakash23@lpu.in>
2026-05-21 10:07:59 +0530

    Fix env variable names

--- proj/settings.py
+++ proj/settings.py
@@ -104,11 +104,11 @@ import os
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'defaultdb',
-        'USER': 'avnadmin',
-        'PASSWORD': 'AVNS_FMh1XEDNz3b8ZxUMt-r',
-        'HOST': 'mysql-3611e7b0-hariofficial965-3173.a.aivencloud.com',
-        'PORT': '27892',
+        'NAME': os.environ.get('DB_NAME', ''),
+        'USER': os.environ.get('DB_USER', ''),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
+        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
 
