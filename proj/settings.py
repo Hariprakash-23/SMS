4fa62c09adfd539d5ed9e4f18145b78b2815007b
Hariprakash-23 <kahariprakash23@lpu.in>
2026-05-21 10:16:42 +0530

    fixed github secrets

--- proj/settings.py
+++ proj/settings.py
@@ -107,8 +107,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME', ''),
         'USER': os.environ.get('DB_USER', ''),
         'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DB_PORT', '3306'),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', ''),
     }
 }
 
